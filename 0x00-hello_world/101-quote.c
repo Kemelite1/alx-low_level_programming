9aea244d449c5b77785baf0029dd68f3bb7a2cc4
Kemelite1 <osikemelite42@gmail.com>
2023-02-16 15:17:34 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - this is a C program that prints a line to the standard error
- * Return 1 when sucessful
+ * main - emtry point
+ *
+ * Description - this is a C program that prints a line to the standard error
+ *
+ * Return: 1 when sucessful
  */
 int main(void)
 {
