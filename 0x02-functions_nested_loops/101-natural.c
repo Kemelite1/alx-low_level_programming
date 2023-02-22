e408173156866ce11e5950dedb7d691dfbd60f57
Kemelite1 <osikemelite42@gmail.com>
2023-02-21 17:42:08 -0800

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - main function
- *
+ * main - this is the main function
  * Return: 0 when successful
  */
 int main(void)
@@ -20,5 +19,4 @@ int main(void)
 	}
 	printf("%d\n", sum);
 	return (0);
-
 }
