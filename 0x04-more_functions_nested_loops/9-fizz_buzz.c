4549c9a23916a0154f8d418a46405fc07448597e
Kemelite1 <osikemelite42@gmail.com>
2023-02-23 17:47:58 -0800

    FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * prints the fizz-Buzz test
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 1; m <= 100; m++)
+	{
+		if (m % 3 == 0 && m % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (m % 5 == 0 && m % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (m % 3 == 0 && m % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (m == 1)
+		{
+			printf("%d", m);
+		} else
+		{
+			printf(" %d", m);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
