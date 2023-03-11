61cf6f22932e0f23a3df3ef286c8a5201e8cd713
Kemelite1 <osikemelite42@gmail.com>
2023-03-10 16:21:46 -0800

    argument count, argument vector

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+
+#ifndef MAIN_H
+		
+#define MAIN_H
+		
+#include <stdio.h>
+		
+int _putchar(char c);
+		
+int _atoi(char *s);
+int check_num(char *str);
+int main(int argc, char *argv[]);
+
+		
+		
+#endif
