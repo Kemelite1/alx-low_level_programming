de9e2b4ab4d1597849da55806fdbc4b962bab0b9
Kemelite1 <osikemelite42@gmail.com>
2023-03-14 18:27:10 -0700

    concatenates all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,19 +14,24 @@ char *argstostr(int ac, char **av)
 	char *str;
 
 	if (ac == 0 || av == NULL)
+
 		return (NULL);
 
 	for (row = 0; row < ac; row++)
 	{
 		len = strlen(av[row]);
+
 		total_len += len + 1;
 	}
 
 	str = (char *) malloc(total_len * sizeof(char));
+
 	if (str == NULL)
+
 		return (NULL);
 
 	len = 0;
+
 	for (row = 0; row < ac; row++)
 	{
 		for (column = 0; av[row][column] != '\0'; column++)
@@ -36,5 +41,6 @@ char *argstostr(int ac, char **av)
 		str[len++] = '\n';
 	}
 	str[len] = '\0';
+
 	return (str);
 }
