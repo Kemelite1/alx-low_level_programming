0e2748c53099c98d777d6c5a2e35f8e8189fb5ad
Kemelite1 <osikemelite42@gmail.com>
2023-04-10 19:37:36 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,55 +1,96 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
-#define BUFFER_SIZE 1024
+void error(char *message, char *file);
+void close_file(int fd);
+
 /**
- * copy_file - allocates 1024 bytes to a bufer
- * @dest_file: name of file buffer is storing chars
- * Return: pointer to newly allocated buffer
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
-
-void copy_file(char *src_file, char *dest_file)
-{
-	FILE *src, *dest;
-	char buffer[BUFFER_SIZE];
-	size_t bytes_read;
-
-	src = fopen(src_file, "r");
-	if (src == NULL) 
-	{
-	printf("Error: Can't read from file %s\n", src_file);
-	exit(98);
-	}
-
-	dest = fopen(dest_file, "w");
-	if (dest == NULL)
-	{
-		printf("Error: Can't write to %s\n", dest_file);
-		exit(99);
-	}
-
-	while ((bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, src)) > 0) 
-	{
-		size_t bytes_written = fwrite(buffer, sizeof(char), bytes_read, dest);
-		if (bytes_written < bytes_read) 
-		{
-			printf("Error: Incomplete write to %s\n", dest_file);
-			exit(99);
-		}
-	}
-
-	fclose(src);
-	fclose(dest);
-}
-
 int main(int argc, char *argv[])
 {
+	int from, to, r, w;
+	char buffer[1024];
+
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
-		exit(97);
+		error("Usage: cp file_from file_to\n", NULL);
+	}
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+	{
+		error(NULL, argv[1]);
+	}
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		error(NULL, argv[2]);
 	}
 
-	copy_file(argv[1], argv[2]);
-	return 0;
+	do {
+	r = read(from, buffer, sizeof(buffer));
+	if (r == -1)
+	{
+		error(NULL, argv[1]);
+	}
+
+	w = write(to, buffer, r);
+	if (w == -1)
+	{
+		error(NULL, argv[2]);
+	}
+	} while (r > 0);
+
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
+
+/**
+ * error - Prints an error message to standard error.
+ * @message: The error message to print.
+ * @file: The file that caused the error, or NULL if not applicable.
+ */
+void error(char *message, char *file)
+{
+	if (errno != 0)
+	{
+		perror("Error");
+	}
+	else if (message != NULL)
+	{
+		fprintf(stderr, "Error: %s", message);
+	}
+	if (file != NULL)
+	{
+		fprintf(stderr, " %s", file);
+	}
+	fprintf(stderr, "\n");
+	exit(errno != 0 ? errno : 1);
+}
+
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to close.
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		error(NULL, NULL);
+	}
 }
